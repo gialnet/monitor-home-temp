9b0ec71b55bdd3a832385630f7e37e2b5a215ac0
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2017-04-28 17:15:53 +0200

    proyecto netbeans

---
+++ src/main/webapp/SQL/CreateTable.sql
@@ -0,0 +1,34 @@
+
+/**
+ * Author:  antonio
+ * Created: 28-abr-2017
+ */
+
+/*
+createdb monitor-ht -E UTF8 -l es_ES.UTF-8
+
+CREATE USER dilar_monitor PASSWORD 'Lupita#2017#';
+
+ALTER USER dilar_monitor WITH SUPERUSER;
+
+*/
+
+CREATE TABLE Devices
+(
+    device_uid          varchar(90) NOT NULL,
+    tipo                text,
+    location            text,
+    fecha_alta          timestamp default now(),
+    primary key (device_uid)
+);
+
+
+
+
+CREATE TABLE measurements
+(
+    id                  serial NOT NULL,
+    uid                 varchar(90) references Devices(device_uid),
+    fecha_hora          timestamp default now(),
+    primary key (id)
+);
